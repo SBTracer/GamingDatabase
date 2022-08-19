8eebf9af3faeaf57f1045ece854642420ff10742
SBTracer <jmwilliamson2014@gmail.com>
2022-08-19 12:10:00 -0500

    Update greeting.js
    Modified criteria for trigger

--- js/greeting.js
+++ js/greeting.js
@@ -7,7 +7,7 @@ var greeting;
 
 if (hournow > 18) {
     greeting = 'Good Evening!';
-} else if (hournow > 12) {
+} else if (hournow >= 12) {
     greeting = 'Good Afternoon!';
 } else if (hournow > 0) {
     greeting = 'Good Morning!';
